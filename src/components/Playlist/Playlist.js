c3ef6b0e8e6fbf78869f2b2ee810b5c60fce1d1d
camel1028 <ariqshah56@gmail.com>
2022-12-15 02:51:05 -0500

    css imports and minor bug fixes

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Playlist.css";
+import TrackList from "../TrackList/TrackList";
 
 function Playlist() {
     return (
         <div className="Playlist">
-            <input value="New Playlist"/>
-            <p> Add a TrackList component </p>
+            <input defaultValue={'New Playlist'}/>
+            Track List goes here
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     );
